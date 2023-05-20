164154049d4d8e87cef2827aa2d7a9e46b3d6392
kimwooyoung <whoyoung90@naver.com>
2023-05-21 00:20:46 +0900

    study: 230520 asyncData 속성안내 및 코드 수정

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Nuxt-exercise',
+    title: 'learn-nuxt',
     htmlAttrs: {
       lang: 'en',
     },
@@ -34,4 +34,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  // server setup
+  server: {
+    port: 8080,
+  },
 }
--- pages/main.vue
+++ pages/main.vue
@@ -1,9 +1,35 @@
 <template>
   <div>
     <p>메인 페이지입니다</p>
+    <div>{{ products }}</div>
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+
+export default {
+  /**
+   * @decription asyncData
+   * "페이지가 그려지기 전에" 호출되는 속성
+   * asyncData 안에 로직이 잘못되면 당연히 페이지 진입이 안된다!
+   * 페이지 진입시 화면깜박임(CSR)이 사라진다!
+   */
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    // this.products = response.data
+    const products = response.data
+    return { products } // 리턴값이 vue의 인스턴스 데이터와 동일하게 동작
+  },
+
+  // data() {
+  //   return {
+  //     products: [],
+  //   }
+  // },
+  // async created() {
+  //   const response = await axios.get('http://localhost:3000/products')
+  //   this.products = response.data
+  // },
+}
 </script>
