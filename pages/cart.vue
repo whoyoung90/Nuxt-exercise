54d61de978cda853ed96ed0932cf31dfc1b83d83
kimwooyoung <whoyoung90@naver.com>
2023-07-05 00:23:43 +0900

    study: 230704 nuxtServerInit 소개

--- pages/cart.vue
+++ pages/cart.vue
@@ -27,13 +27,13 @@
 </template>
 
 <script>
-// store에서 상수화를 하면 import해서 쓸 수 있다!
+// store에서 action/mutation 함수를 상수화 하면 import해서 쓸 수 있다!
 import { FETCH_CART_ITEMS } from '@/store'
 
 export default {
   async asyncData({ store }) {
-    // async - await를 설정하지 않으면 "데이터를 받기도 전에 페이지에 진입"하겠죠? 데이터 빈 상테..
-    await store.dispatch(FETCH_CART_ITEMS)
+    // await를 하지 않으면, 비동기 요청에서 "데이터를 받기도 전에 페이지에 진입"하겠죠? 데이터 빈 상테..
+    await store.dispatch(FETCH_CART_ITEMS) // 데이터를 받아올 때까지 await
   },
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -2,9 +2,11 @@ import { fetchCartItems } from '@/api'
 
 /**
  * @description constants
+ * action/mutation 함수를 별도로 상수화 시켜서 그 상수를 import하는 방식!
  * 동일한 action/mutation 함수를 여러번 호출할 때 사용
+
  * 문자열 값이 "변수"로 관리될 때, 변수의 값을 action/mutation 함수에 연결
- * 변수의 이름과 문자열의 이름이 같기 때문에, 문자열을 동적으로 연결해주는 문법
+ * (문자열을 동적으로 연결해주는 문법 ✨)
  */
 export const FETCH_CART_ITEMS = 'FETCH_CART_ITEMS'
 
@@ -27,7 +29,7 @@ export const mutations = {
 }
 
 export const actions = {
-  // 동적 키 정의 방식 (ES6+)
+  // 동적 키값 정의 방식 (ES6+) ✨
   async [FETCH_CART_ITEMS]({ commit }) {
     const { data } = await fetchCartItems()
     commit('setCartItems', data)
